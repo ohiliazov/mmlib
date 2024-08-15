df062a340af300d0c5942bf8eff8b8391b11a0ee
Oleksandr Hiliazov <oleksandr.hiliazov@robivision.eu>
2024-08-15 13:07:03 +0200

    refactor

--- mmlib/seeding.py
+++ mmlib/seeding.py
@@ -1,16 +1,17 @@
 from mmlib.constants import SeedingMode
 
 
-def _validate_seeding_arguments(
+def _sanitize_seeding_arguments(
     p1_idx: int,
     p2_idx: int,
     group_size: int,
-) -> None:
+) -> tuple[int, int, int]:
     if group_size < 2:
         raise ValueError("`group_size` must be a greater or equal to 2.")
 
     if p1_idx > p2_idx:
-        raise ValueError("`p1_idx` must be less than `p2_idx`.")
+        p1_idx, p2_idx = p2_idx, p1_idx
+    group_size += group_size % 2
 
     if not 0 <= p1_idx < group_size:
         raise ValueError(f"`p1_idx` must be between 0 and {group_size-1}.")
@@ -18,30 +19,28 @@ def _validate_seeding_arguments(
     if not 0 <= p2_idx < group_size:
         raise ValueError(f"`p2_idx` must be between 0 and {group_size-1}.")
 
+    return p1_idx, p2_idx, group_size
+
 
 def seeding_cross(p1_idx: int, p2_idx: int, size: int) -> float:
-    _validate_seeding_arguments(p1_idx, p2_idx, size)
-    size += size % 2
+    p1_idx, p2_idx, size = _sanitize_seeding_arguments(p1_idx, p2_idx, size)
     if size == 2:
         return 1
-    return 1 - abs(2 * abs(p1_idx - p2_idx) - size) / (size - 2)
+    return 1 - abs(2 * (p2_idx - p1_idx) - size) / (size - 2)
 
 
 def seeding_fold(p1_idx: int, p2_idx: int, size: int) -> float:
-    _validate_seeding_arguments(p1_idx, p2_idx, size)
-    size += size % 2
+    p1_idx, p2_idx, size = _sanitize_seeding_arguments(p1_idx, p2_idx, size)
     if size == 2:
         return 1
-
     return 1 - abs(p2_idx + p1_idx - size + 1) / (size - 2)
 
 
 def seeding_adjacent(p1_idx: int, p2_idx: int, size: int) -> float:
-    _validate_seeding_arguments(p1_idx, p2_idx, size)
-    size += size % 2
+    p1_idx, p2_idx, size = _sanitize_seeding_arguments(p1_idx, p2_idx, size)
     if size == 2:
         return 1
-    return 1 - (abs(p1_idx - p2_idx) - 1) / (size - 2)
+    return 1 - (p2_idx - p1_idx - 1) / (size - 2)
 
 
 def seeding_coefficient(
